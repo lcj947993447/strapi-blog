cf91be8ffef44f67d5698384657fe089b934f38a
lcj947993447 <liangchengjian0510@gmail.com>
2020-11-14 14:20:18 +0800

    add

--- config/database.js
+++ config/database.js
@@ -1,32 +1,32 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'mysql',
-//         host: env('DATABASE_HOST', 'localhost'),
-//         port: env.int('DATABASE_PORT', 3306),
-//         database: env('DATABASE_NAME', 'blog'),
-//         username: env('DATABASE_USERNAME', 'root'),
-//         password: env('DATABASE_PASSWORD', 'lcj19960510#'),
-//       },
-//       options: {},
-//     }
-//   }
-// });
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'mysql',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'blog'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', 'lcj19960510#'),
       },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-});
+      options: {},
+    }
+  }
+})
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       }
+//     }
+//   }
+// })
